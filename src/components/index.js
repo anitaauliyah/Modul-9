f5a5624bc13a5d31fb1827b4f2a3e60890888f2a
anitaauliyah <anitaauliyah526@gmail.com>
2023-12-14 13:33:43 +0700

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import BottomNavigator from './besar/BottomNavigator'
+import ListNote from './besar/ListNote'
+import TabItem from './besar/TabItem'
+
+export default {BottomNavigator, ListNote, TabItem}
+export * from './kecil'
---
+++ src/config/FIREBASE/index.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBMDHkueAaL9sY_ha03y3GCrrqW13Cz5o4",
+  authDomain: "modul-9-222be.firebaseapp.com",
+  databaseURL: "https://modul-9-222be-default-rtdb.firebaseio.com",
+  projectId: "modul-9-222be",
+  storageBucket: "modul-9-222be.appspot.com",
+  messagingSenderId: "889710035596",
+  appId: "1:889710035596:web:ee34bca845bb20285431a9"
+};
+
+const FIREBASE = firebase;
+
+export default FIREBASE;
